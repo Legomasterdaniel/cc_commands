11ad3f660adf15bdd0a291ba9ef7f0ddb995816e
Legomasterdaniel <gamerdaniel@tntcomp.com>
2025-11-30 10:56:28 -0500

    Update terminal.lua

--- src/cc_commands/command_executors/terminal.lua
+++ src/cc_commands/command_executors/terminal.lua
@@ -1,8 +1,19 @@
 sleep(1)
-while (true) do
+while true do
+    term.clear()
+    term.setCursorPos(1, 1)
     write("Options:\n - help | Lists out all available commands.")
     write("Input a command: ")
     local input = read()
-    print(input)
     
+    if fs.exists("cc_commands/commands/"..input) then
+        pcall(function()
+            os.run({}, "cc_commands/commands/"..input)
+        end)
+    end
+    print("\nPress any key to continue..")
+    while true do
+        local event = os.pullEvent()
+        if event == "key" then break end
+    end
 end

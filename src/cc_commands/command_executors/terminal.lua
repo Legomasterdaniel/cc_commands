73ca78b9576dd097fd7d0c5eb7d5ae2f292d9d9c
Legomasterdaniel <gamerdaniel@tntcomp.com>
2025-11-30 10:28:06 -0500

    Update terminal.lua

--- src/cc_commands/command_executors/terminal.lua
+++ src/cc_commands/command_executors/terminal.lua
@@ -1,6 +1,6 @@
 while (true) do
     write("Options:\n - help | Lists out all available commands.")
-    write("Input a command: ")
+    write("Input a comman: ")
     local input = read()
     print(input)
     

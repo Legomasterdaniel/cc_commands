926aaf75873ae566bc3d9d140187e8547ff3bac7
Legomasterdaniel <gamerdaniel@tntcomp.com>
2025-11-30 08:46:58 -0500

    Update startup.lua

--- src/startup.lua
+++ src/startup.lua
@@ -1,6 +1,6 @@
 local githubData = {
-    UrlBase = "https://raw.githubusercontent.com/repos/%s/%s/refs/heads/master/%s",
-    Owner = "legomasterdaniel",
+    UrlBase = "https://raw.githubusercontent.com/repos/%s/%s/refs/heads/main/%s",
+    Owner = "Legomasterdaniel",
     Repository = "cc_commands"
 }
 
@@ -9,17 +9,17 @@ local function CreateDirectory(path)
 end
 local function InsertFromRepository(path, githubFilePath)
     local request = http.get(string.format(
-        githubData.UrlBase,
-        "legomasterdaniel", "cc_commands", filePath
-    ))
+        githubData.UrlBase, githubData.Owner, githubData.Repository, filePath))
     local fileData = request.readAll()
     request.close()
     
-
+    local file = fs.open(path, "w")
+    file.write(fileData)
+    file.close()
 end
 
 local function IsInstalled()
-    if fs.exists("/rom/cc_commands") then return true end
+    if fs.exists("rom/cc_commands") then return true end
     return false
 end
 
@@ -27,13 +27,15 @@ local function CreateFiles()
     if IsInstalled() then return end -- if already set up, stop process.
 
     -- Make necessary directories.
-    CreateDirectory("/rom/cc_commands")
-    CreateDirectory("/rom/cc_commands/commands")
+    CreateDirectory("rom/cc_commands")
+    CreateDirectory("rom/cc_commands/commands")
 
     -- Install the lua files from pastebin.
     pastebin get 
 end
 
 if IsInstalled() then
+    os.run(shell.resolve("rom/cc_commands/startup.lua")) -- Run normal command
+else
 
 end
